7a7c40c097e1ecb41d81ee43ba1c9c89fb6195a8
hrayleung <hinrayleung@gmail.com>
2025-04-26 23:29:24 +0800

    250426

--- notes_DP/word_warp.cpp
+++ notes_DP/word_warp.cpp
@@ -36,7 +36,7 @@ void word_warp(int m, vector<string> w) {
     }
 
     vector<vector<long long>> cost(n + 1, vector<long long>(n + 1, inf));
-    for (int i = 0; i <= n; i++) {
+    for (int i = 1; i <= n; i++) {
         for (int j = i; j <= n; j++) {
             int l = s[j] - s[i - 1] + j - i;
             int e = m - l;
@@ -54,11 +54,33 @@ void word_warp(int m, vector<string> w) {
     dp[0] = 0;
     for (int j = 1; j <= n; j++) {
         for (int i = 1; i <= j; i++) {
-            if (dp[i - 1] != inf && dp[i - 1] + cost[i][j] < dp[j]) {
+            if (dp[i - 1] != inf && cost[i][j] != inf && dp[i - 1] + cost[i][j] < dp[j]) {
                 dp[j] = dp[i - 1] + cost[i][j];
                 p[j] = i;
             }
         }
     }
 
+    // Reconstruct the solution using backtracking
+    vector<vector<int>> solution;
+    int j = n;
+    while (j > 0) {
+        int i = p[j];
+        vector<int> line;
+        for (int k = i; k <= j; k++) {
+            line.push_back(k - 1); // Convert to 0-based indexing
+        }
+        solution.push_back(line);
+        j = i - 1;
+    }
+    reverse(solution.begin(), solution.end());
+
+    // Print the solution
+    for (const auto& line : solution) {
+        for (size_t i = 0; i < line.size(); i++) {
+            if (i > 0) cout << " ";
+            cout << w[line[i]];
+        }
+        cout << endl;
+    }
 }
