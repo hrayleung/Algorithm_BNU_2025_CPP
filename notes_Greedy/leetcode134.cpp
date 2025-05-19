ac86d31422bc604eb7024fd4cc386abe0736c972
hrayleung <hinrayleung@gmail.com>
2025-05-19 14:18:49 +0800

    250519

--- notes_Greedy/leetcode134.cpp
+++ notes_Greedy/leetcode134.cpp
@@ -12,17 +12,28 @@ int main() {
     int n;
     cin >> n;
     vector<int> gas(n), cost(n);
+    for (int i = 0; i < n; i++) {
+        cin >> gas[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> cost[i];
+    }
+    cout << canCompleteCircuit(gas, cost) << endl;
 }
 
 int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
     int size = gas.size();
     int start = 0;
-    vector<int> tank(size);
+    int total = 0;
+    int curr = 0;
     for (int i = 0; i < size; i++) {
-        tank[i] = gas[i] - cost[i];
-        if (tank[i] < 0) {
-            start = start + i + 1;
+        int diff = gas[i] - cost[i];
+        total += diff;
+        curr += diff;
+        if (curr < 0) {
+            start = i + 1;
+            curr = 0;
         }
-
     }
+    return total >= 0 ? start : -1;
 }
