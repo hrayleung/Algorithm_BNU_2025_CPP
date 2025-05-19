ff66e5963466fae15b6e5d055509174bed6b9eba
hrayleung <hinrayleung@gmail.com>
2025-05-19 17:10:27 +0800

    Update lab04 files and add leetcode134 solution

--- lab04/lab0403.cpp
+++ lab04/lab0403.cpp
@@ -8,7 +8,8 @@ using namespace std;
 
 #define mod 100007
 
-struct Node {
+struct Node
+{
     int freq;
     char c;
     int id;
@@ -18,30 +19,43 @@ struct Node {
     Node(int f, char ch = '\0', int i = 0) : freq(f), c(ch), id(i), left(nullptr), right(nullptr) {}
 };
 
-struct CompareNode {
-    bool operator()(Node *a, Node *b) {
-        if (a->freq != b->freq) {
+struct CompareNode
+{
+    bool operator()(Node *a, Node *b)
+    {
+        if (a->freq != b->freq)
+        {
             return a->freq > b->freq;
         }
 
         bool a_is_leaf = (a->c != '\0');
         bool b_is_leaf = (b->c != '\0');
 
-        if (a_is_leaf && b_is_leaf) {
+        if (a_is_leaf && b_is_leaf)
+        {
             return a->c > b->c;
-        } else if (a_is_leaf) {
+        }
+        else if (a_is_leaf)
+        {
             return false;
-        } else if (b_is_leaf) {
+        }
+        else if (b_is_leaf)
+        {
             return true;
-        } else {
+        }
+        else
+        {
             return a->id > b->id;
         }
     }
 };
 
-void buildHuffmanCodes(Node *root, string code, unordered_map<char, string> &huffmanCode) {
-    if (root == nullptr) return;
-    if (root->c != '\0') {
+void buildHuffmanCodes(Node *root, string code, unordered_map<char, string> &huffmanCode)
+{
+    if (root == nullptr)
+        return;
+    if (root->c != '\0')
+    {
         huffmanCode[root->c] = code;
     }
 
@@ -49,12 +63,14 @@ void buildHuffmanCodes(Node *root, string code, unordered_map<char, string> &huf
     buildHuffmanCodes(root->right, code + "1", huffmanCode);
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     vector<int> frequencies(26);
-    for (int i = 0; i < 26; i++) {
+    for (int i = 0; i < 26; i++)
+    {
         cin >> frequencies[i];
     }
 
@@ -63,14 +79,17 @@ int main() {
 
     priority_queue<Node *, vector<Node *>, CompareNode> pq;
 
-    for (int i = 0; i < 26; i++) {
-        if (frequencies[i] > 0) {
+    for (int i = 0; i < 26; i++)
+    {
+        if (frequencies[i] > 0)
+        {
             pq.push(new Node(frequencies[i], 'a' + i));
         }
     }
 
     int id_counter = 0;
-    while (pq.size() > 1) {
+    while (pq.size() > 1)
+    {
         Node *left = pq.top();
         pq.pop();
         Node *right = pq.top();
@@ -89,7 +108,8 @@ int main() {
     buildHuffmanCodes(root, "", huffmanCode);
 
     long long totalBits = 0;
-    for (char ch : text) {
+    for (char ch : text)
+    {
         totalBits = (totalBits + huffmanCode[ch].length()) % mod;
     }
 
